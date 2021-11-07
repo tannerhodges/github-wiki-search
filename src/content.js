8e4d17deef1a352a29ab47b2c57e5a44d4429e87
Tanner Hodges <me@tannerhodges.com>
2021-11-07 10:58:14 -0500

    Fix search appearance in Dark mode

--- src/content.js
+++ src/content.js
@@ -23,8 +23,9 @@
     // Add search form to the top of the sidebar.
     sidebar.insertAdjacentHTML('afterbegin', `
       <form id="github-wiki-search" class="Box Box--condensed box-shadow mb-4" action="${search}" method="GET">
-        <div style="margin: -1px -1px 0; padding: 10px; background-color: #f6f8fa; border: 1px solid #e1e4e8; border-top-left-radius: 6px; border-top-right-radius: 6px;">
-          <input class="form-control input-sm input-block" type="text" name="q" placeholder="Search wiki…" aria-label="Search wiki…" autocomplete="off">
+        <div class="Box-header">
+          <h3 class="Box-title mb-2">Search Wiki</h3>
+          <input class="form-control input-sm input-block" type="text" name="q" placeholder="Enter keywords…" aria-label="Enter keywords…" autocomplete="off">
           <input type="hidden" name="type" value="Wikis">
         </div>
       </form>
