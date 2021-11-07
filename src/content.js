dc2721f53690173992df3fd3830a4ee5a0214c5b
Tanner Hodges <me@tannerhodges.com>
2021-11-07 11:08:45 -0500

    🌗 Fix Dark Mode + PJAX Page Loads (#1)
    * Wait to init until DOMContentLoaded
    
    * Fix wiki not being added after PJAX page loads
    
    * Fix search appearance in Dark mode
    
    * Add changelog notes
    
    * Update npm dependencies
    
    * 1.0.2

--- src/content.js
+++ src/content.js
@@ -1,22 +1,44 @@
 (() => {
-  // Find the wiki sidebar.
-  const sidebar = document.querySelector('.wiki-rightbar');
+  /**
+   * Init GitHub Wiki Search.
+   * @return void
+   */
+  function init() {
+    // Find the wiki sidebar.
+    const sidebar = document.querySelector('.wiki-rightbar');
 
-  // If there isn't a sidebar, this isn't a wiki page. Exit!
-  if (!sidebar) {
-    return;
+    // If there isn't a sidebar, this isn't a wiki page. Exit!
+    if (!sidebar) {
+      return;
+    }
+
+    // If the sidebar already has a wiki search, abort!
+    if (sidebar.querySelector('#github-wiki-search')) {
+      return;
+    }
+
+    // Get search URL for the current repo.
+    const search = window.location.pathname.replace(/(\/.*?\/.*?\/)wiki.*/, '$1search');
+
+    // Add search form to the top of the sidebar.
+    sidebar.insertAdjacentHTML('afterbegin', `
+      <form id="github-wiki-search" class="Box Box--condensed box-shadow mb-4" action="${search}" method="GET">
+        <div class="Box-header">
+          <h3 class="Box-title mb-2">Search Wiki</h3>
+          <input class="form-control input-sm input-block" type="text" name="q" placeholder="Enter keywords…" aria-label="Enter keywords…" autocomplete="off">
+          <input type="hidden" name="type" value="Wikis">
+        </div>
+      </form>
+    `);
   }
 
-  // Get search URL for the current repo.
-  const search = window.location.pathname.replace(/(\/.*?\/.*?\/)wiki.*/, '$1search');
+  // Initialize on DOMContentLoaded.
+  if (document.readyState !== 'loading') {
+    init();
+  } else {
+    document.addEventListener('DOMContentLoaded', init);
+  }
 
-  // Add search form to the top of the sidebar.
-  sidebar.insertAdjacentHTML('afterbegin', `
-    <form class="Box Box--condensed box-shadow mb-4" action="${search}" method="GET">
-      <div style="margin: -1px -1px 0; padding: 10px; background-color: #f6f8fa; border: 1px solid #e1e4e8; border-top-left-radius: 6px; border-top-right-radius: 6px;">
-        <input class="form-control input-sm input-block" type="text" name="q" placeholder="Search wiki…" aria-label="Search wiki…" autocomplete="off">
-        <input type="hidden" name="type" value="Wikis">
-      </div>
-    </form>
-  `);
+  // Initialize again after PJAX page loads.
+  window.addEventListener('pjax:end', init);
 })();
